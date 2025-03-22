93480f5f37b8fcc5b1059830679c09fc51ef55f5
Eii3 <jeungbernice@outlook.com>
2025-03-22 18:27:46 +0800

    互斥锁解决商铺查询的缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -33,6 +34,84 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        // 解决缓存穿透
+        //Shop shop = queryWithPassThrough(id);
+
+        // 解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if(shop == null){
+            return Result.fail("店铺不存在");
+        }
+
+        // 5.返回商铺信息
+        return Result.ok(shop);
+    }
+
+    // 互斥锁解决缓存击穿
+    public Shop queryWithMutex(Long id){
+        String key = CACHE_SHOP_KEY + id;
+        // 1.从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2.判断是否存在
+        //isNotBlank: null、""、"\t\n"为false
+        if(StrUtil.isNotBlank(shopJson)){
+            // 3.存在 直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        // 存在但为空值
+        if(shopJson != null){
+            // 返回一个错误信息
+            return null;
+        }
+
+        // 4.不存在 实现缓存重建
+        // 4.1.实现互斥锁
+        String lockKey = "lock:shop:"+id;
+        Shop shop = null;
+        try {
+            // 4.2.判断是否获取锁
+            if(!tryLock(lockKey)){
+                // 4.3.获取失败 休眠重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+
+            // 4.4.获取成功 根据id查询数据库
+            shop = getById(id);
+
+            // 5.不存在 返回错误
+            if(shop == null){
+                // 将空值写入redis
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+                return null;
+            }
+
+            // 6.存在 写入redis
+            stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            // 这是个打断异常 不用做处理
+            throw new RuntimeException(e);
+        } finally {
+            // 7.释放互斥锁
+            unlock(lockKey);
+        }
+
+        // 8.返回商铺信息
+        return shop;
+    }
+
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key,"1",10,TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+    // 封装解决缓存穿透的代码
+    public Shop queryWithPassThrough(Long id){
         String key = CACHE_SHOP_KEY + id;
         // 1.从redis查询商铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(key);
@@ -40,13 +119,12 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //isNotBlank: null、""、"\t\n"为false
         if(StrUtil.isNotBlank(shopJson)){
             // 3.存在 直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSONUtil.toBean(shopJson, Shop.class);
         }
         // 判断是否命中空值
         if(shopJson != null){
             // 返回一个错误信息
-            return Result.fail("店铺信息不存在");
+            return null;
         }
         // 4.不存在 根据id查询数据库
         Shop shop = getById(id);
@@ -55,13 +133,16 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             // 4.2.不存在 返回404
             // 将空值写入redis
             stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
-            return Result.fail("店铺不存在");
+            return null;
         }
         // 4.3.存在 存入redis
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 5.返回商铺信息
-        return Result.ok(shop);
+        return shop;
     }
+
+
+
 
     @Override
     @Transactional //开启事务
